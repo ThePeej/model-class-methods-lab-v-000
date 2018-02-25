80f5572cc328df733fab49573c84a3daffa1e040
PJ Frias <peejfrias@gmail.com>
2018-02-25 21:54:23 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -16,7 +16,7 @@ class Boat < ActiveRecord::Base
   end
 
   def self.last_three_alphabetically
-    self.all.sort_by(:name)[0-2]
+    self.all.sort_by(:name).limit(2)
   end
 
   def self.without_a_captain
